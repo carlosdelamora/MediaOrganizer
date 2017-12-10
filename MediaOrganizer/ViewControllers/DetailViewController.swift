76c751251d1296e5faa88ca8d220499c46c39483
Carlos De la Mora <carlosdelamorac@gmail.com>
2017-12-09 21:53:42 -0700

    feat: send media

--- MediaOrganizer/ViewControllers/DetailViewController.swift
+++ MediaOrganizer/ViewControllers/DetailViewController.swift
@@ -23,6 +23,11 @@ class DetailViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //create an item to share the media
+        let shareMedia = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(presentActionController))
+        navigationItem.rightBarButtonItem = shareMedia
+        
         imageView.placeSquareImageFromMedia(media: media)
         switch media.stringMediaType{
         case Constants.mediaType.photo:
@@ -32,6 +37,52 @@ class DetailViewController: UIViewController{
         case Constants.mediaType.video:
             let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(playMovie))
             contentView.addGestureRecognizer(gestureRecognizer)
+        default:
+            print("this should not happen ")
+            break
+        }
+    }
+    
+    @objc func presentActionController(){
+        
+        switch media.stringMediaType{
+        case Constants.mediaType.photo:
+            guard let image = imageView.image else{return}
+            let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            present(activityController, animated: true)
+        case Constants.mediaType.video:
+            if media.isPhAsset{
+                guard let phAsset = media.getPhAsset() else{ return }
+                PHImageManager.default().requestPlayerItem(forVideo: phAsset, options: nil, resultHandler: { playerItem, info in
+                    if let playerAsset = playerItem?.asset as? AVURLAsset{
+                        
+                        let url = playerAsset.url
+                        var videoData = Data()
+                        var tempURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                        tempURL.appendPathComponent("temp.mov")
+                        do{
+                            videoData = try Data(contentsOf: url)
+                        }catch{
+                            print("error with the video data \(error)")
+                        }
+                        
+                        do{
+                            try videoData.write(to: tempURL)
+                        }catch{
+                            print("could not write to tempURL\(error)")
+                        }
+
+                        let activityController = UIActivityViewController(activityItems: [tempURL], applicationActivities: nil)
+                        DispatchQueue.main.async {
+                            self.present(activityController, animated: true)
+                        }
+                    }
+                })
+            }else{
+                let mediaUrl = media.getURL()
+                let activityController = UIActivityViewController(activityItems: [mediaUrl], applicationActivities: nil)
+                present(activityController, animated: true)
+            }
         default:
             print("this should not happen ")
             break
@@ -48,8 +99,6 @@ class DetailViewController: UIViewController{
                playerViewController.player = player
             
            })
-          
-            
         }else{
             let url = media.getURL()
             player = AVPlayer(url: url)
